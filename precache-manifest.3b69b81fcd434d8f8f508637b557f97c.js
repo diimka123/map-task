7f5f6217f2e223ef7fb60238f5fb7a49029ffbfa
Dmitry <riiba228@gmail.com>
2020-11-22 20:42:23 +0300

    deploy: deploy commit

---
+++ precache-manifest.3b69b81fcd434d8f8f508637b557f97c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7484a03fd5c97b682d3690369e5ae28c",
+    "url": "/map-task/index.html"
+  },
+  {
+    "revision": "8a3365c2216a73adfa09",
+    "url": "/map-task/static/css/main.34db4ef7.chunk.css"
+  },
+  {
+    "revision": "a87882f908d36fb57b53",
+    "url": "/map-task/static/js/2.e23bbc42.chunk.js"
+  },
+  {
+    "revision": "9bbc3dd1b9f85f95f5c7cf5509bb7fad",
+    "url": "/map-task/static/js/2.e23bbc42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3365c2216a73adfa09",
+    "url": "/map-task/static/js/main.83ae68f4.chunk.js"
+  },
+  {
+    "revision": "07f8e905617f648c7c73",
+    "url": "/map-task/static/js/runtime-main.5745378f.js"
+  }
+]);
